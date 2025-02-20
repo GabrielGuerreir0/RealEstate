539ed52ee4daaa90274dd68c01acf8a9d05f50ad
douglas <douglasholanda3195@gmail.com>
2025-02-20 16:46:48 -0300

    Criação do footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { NavBarComponent } from './../../components/nav-bar/nav-bar.component';
 import { Component } from '@angular/core';
 import { CarroselImagesComponent } from '../../components/carrosel-images/carrosel-images.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, CarroselImagesComponent],
+  imports: [NavBarComponent, CarroselImagesComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
